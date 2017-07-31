b05d852750454e02dd4e89caeb6b308a0bd19bf3
lagmanzaza <lagmanmc@gmail.com>
2017-08-01 01:18:13 +0700

    [Graphql] add schema for user post review

---
+++ src/graphql/types/userPostReview.js
@@ -0,0 +1,10 @@
+export default `
+    type userReview{
+        userID: String
+        userName: String
+        userImage: String
+        title: String
+        category: String
+        content: String
+    }
+`
