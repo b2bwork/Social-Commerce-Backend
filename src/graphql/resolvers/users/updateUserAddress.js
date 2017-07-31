2ed757aeaeb61ae6c45a13494b6c3a0a8e98f8e2
lagmanzaza <lagmanmc@gmail.com>
2017-08-01 01:15:36 +0700

    beautify code

--- src/graphql/resolvers/users/updateUserAddress.js
+++ src/graphql/resolvers/users/updateUserAddress.js
@@ -1,24 +1,23 @@
 import userAddressModel from '../../../models/users/usersProfile';
 
 export default {
-    Mutation:{
-        updateUserAddress(root , params , options){
-            console.log(params)
+    Mutation: {
+        updateUserAddress(root, params, options) {
             const update = userAddressModel.update({
                 providerUserID: params.userID
-            },{
-            fullNameAddress: params.fullNameAddress,
-            address: params.address,
-            city: params.city,
-            province: params.province,
-            postalCode: params.postalCode,
-            phoneNumber: params.phoneNumber, 
-            }).then((data)=>{
-                return data.ok;
-            }).catch((err)=>{
-                console.log(err);
-            });
-        return {_id:update};
+            }, {
+                    fullNameAddress: params.fullNameAddress,
+                    address: params.address,
+                    city: params.city,
+                    province: params.province,
+                    postalCode: params.postalCode,
+                    phoneNumber: params.phoneNumber,
+                }).then((data) => {
+                    return data.ok;
+                }).catch((err) => {
+                    console.log(err);
+                });
+            return { _id: update };
         }
     }
 }
