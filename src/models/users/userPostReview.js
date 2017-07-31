9a40d8d656cc052eef2a345a505f41a3c97e3d84
lagmanzaza <lagmanmc@gmail.com>
2017-08-01 01:16:56 +0700

    [Mongoose] add schema for user post review

---
+++ src/models/users/userPostReview.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+let userPostReview = new mongoose.Schema({
+    userID: {
+        type: String
+    },
+    userName: {
+        type: String
+    },
+    userImage: {
+        type: String
+    },
+    title: {
+        type: String
+    },
+    category: {
+        type: String
+    },
+    content: {
+        type: String
+    },
+}, {versionKey: false});
+
+export default mongoose.model('usersReview',userPostReview);
